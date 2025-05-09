c1e52597cde9b29dae4737cd60c2818b2ddfffe5
James Xie <115512123+CosmoJX@users.noreply.github.com>
2025-05-08 21:57:28 -0400

    Update fft.h

--- fft.h
+++ fft.h
@@ -16,30 +16,41 @@
 */
 class CircularQueue {
 public:
-    CircularQueue(int *array, int size) : array(array), size(size), len(0), ind(0), sum(0) {}
+    CircularQueue(int *c_array, float32_t *i_array, int size) : 
+        c_array(c_array), i_array(i_array), size(size), 
+        len(0), ind(0), sum(0), intensity(0) {}
 
     // enqueue a value, if queue is full then loop over
-    void enqueue(int val) {
+    void enqueue(int val, float32_t intn) {
         if (len == size) {
             ind = (ind + 1) % size;
-            sum -= array[ind];
+            sum -= c_array[ind];
+            intensity -= i_array[ind];
             sum += val;
-            array[ind] = val;
+            intensity += intn;
+            c_array[ind] = val;
+            i_array[ind] = intn;
         } else {
-            array[ind++] = val;
+            c_array[ind] = val;
+            i_array[ind++] = intn;
             sum += val;
+            intensity += intn;
             ++len;
         }
     }
 
     int get_sum(void) { return sum; }
+    
+    float32_t get_intensity(void) { return intensity; }
 
 private:
+    int *c_array;           // count array
+    float32_t *i_array;     // intensity array
+    int size;
     int len;
     int ind;
     int sum;
-    int size;
-    int *array;
+    float32_t intensity;
 };
 
 void run_fft();
